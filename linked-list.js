3f647b50fac3de0093a08050934232664e7ea6aa
edwinkim97 <edwinkim124@gmail.com>
2022-04-11 14:53:50 -0700

    finished problem #8

--- linked-list.js
+++ linked-list.js
@@ -65,37 +65,133 @@ class LinkedList {
   /** pop(): return & remove last item. */
 
   pop() {
+    // empty list 
+    if (this.head === null) {
+      return null;
+    }
 
+    let current = this.head;
+
+    // Only one item in the list
+    if (this.head.next === null) {
+      this.head = null;
+      this.tail = null;
+      this.length -= 1;
+      return current.val;
+    }
+
+    while (current.next !== this.tail) {
+      current = current.next;  
+    }
+    const poppedOff = current.next.val;
+    current.next = null;
+    this.tail = current;
+    this.length -= 1;
+    return poppedOff;
   }
 
   /** shift(): return & remove first item. */
 
   shift() {
+    // empty list 
+    if (this.head === null) {
+      return null;
+    }
 
+    let current = this.head;
+
+    // Only one item in the list
+    if (this.head.next === null) {
+      this.head = null;
+      this.tail = null;
+      this.length -= 1;
+      return current.val;
+    }
+
+    this.head = this.head.next;
+    this.length -= 1;
+    return current.val;
   }
 
   /** getAt(idx): get val at idx. */
 
   getAt(idx) {
+    // empty list
+    if (this.head === null) {
+      return null;
+    }
 
+    // If idx is greater than length of list
+    if (idx > this.length - 1) {
+      return null;
+    }
+
+    let targetNode = this.head;
+
+    for (let i = 0; i < idx; i++) {
+      targetNode = targetNode.next;
+    }
+    return targetNode.val;
   }
 
   /** setAt(idx, val): set val at idx to val */
 
   setAt(idx, val) {
+    // empty list
+    if (this.head === null) {
+      return null;
+    }
 
+    // If idx is greater than length of list
+    if (idx > this.length - 1) {
+      return null;
+    }
+
+    let targetNode = this.head;
+
+    for (let i = 0; i < idx; i++) {
+      targetNode = targetNode.next;
+    }
+    targetNode.val = val;
   }
 
   /** insertAt(idx, val): add node w/val before idx. */
 
   insertAt(idx, val) {
+    const newNode = new Node(val);
 
+    // empty list
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.length += 1;
+      return;
+    }
+
+    // If idx is greater than length of list
+    if (idx > this.length) {
+      return null;
+    }
+
+    let targetNode = this.head;
+
+    for (let i = 0; i < idx - 1; i++) {
+      targetNode = targetNode.next;
+    }
+
+    if (targetNode === this.tail) {
+      this.tail = newNode;
+    }
+
+    newNode.next = targetNode.next;
+    targetNode.next = newNode;
+    this.length += 1;
   }
 
   /** removeAt(idx): return & remove item at idx, */
 
   removeAt(idx) {
-
+    
   }
 
   /** average(): return an average of all values in the list */
